dc092377ed82bd7fee16d7c1489d6e81884c4846
AI Fairy <70135143@student.uol.edu.pk>
2024-10-27 19:30:57 +0500

    changes

--- lab1f/lib/currency_coverter_page.dart
+++ lab1f/lib/currency_coverter_page.dart
@@ -1,63 +1,99 @@
 import 'package:flutter/material.dart';
-class CurrencyConverterPage extends StatelessWidget{
- const CurrencyConverterPage({super.key});
- @override
- Widget build(BuildContext context ){
-  const border =  OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(60)),
-                borderSide: BorderSide(color:Color.fromARGB(122, 132, 142, 145),
-                width: 2.0,
-                style:BorderStyle.solid, 
-                strokeAlign:BorderSide.strokeAlignOutside
-              ));
-  return  Scaffold(
-    backgroundColor:Colors.white,
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-       const Text(
-         '\$ 0',
-         style:TextStyle(
-           fontSize:23,
-         ),
-        ),
-        const Padding(
-          padding: EdgeInsets.all(10.0),
-          
-          
-          child: TextField(
-            decoration:InputDecoration(
-                hintText:"Hey",
-                hintStyle: TextStyle(color:Color.fromARGB(225,211,178,198)),
-              prefixIcon: Icon(
-                Icons.monetization_on,
-              ),
-              filled:true,
-              fillColor: Color.fromARGB(122, 132, 142, 146),
-              focusedBorder:border,
-              enabledBorder: border,
-              ),
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true,
-              ),
-            ),),
-        TextButton(
-          onPressed:(){
-            debugPrint('pressed');
-          },
-          style:const ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Color.fromARGB(122, 132, 142, 142)),
-            foregroundColor: WidgetStatePropertyAll(Color.fromARGB(122, 132, 142, 142)),
-           
-          ),
-          child:const Text(
-            'converter',
-            style:TextStyle(color:Colors.pink),
-          )
-        )
-      ],
-    ),
+import 'text_display.dart';
+import 'custom_text_field.dart';
+import 'custom_text_button.dart';
 
-  );
- }
+class CurrencyConverterPage extends StatelessWidget {
+  const CurrencyConverterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.purple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+          TextDisplay(),
+          CustomTextField(),
+          CustomTextButton(),
+        ],
+      ),
+    );
+  }
+}import 'package:flutter/material.dart';
+
+class TextDisplay extends StatelessWidget {
+  const TextDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '\$0',
+      style: TextStyle(
+        fontSize: 23,
+      ),
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(10.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Hey Alina",
+          hintStyle: TextStyle(color: Color.fromARGB(255, 211, 178, 198)),
+          prefixIcon: Icon(
+            Icons.monetization_on,
+          ),
+          filled: true,
+          fillColor: Color.fromARGB(122, 132, 142, 146),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            borderSide: BorderSide(
+                color: Color.fromARGB(255, 175, 171, 175),
+                width: 2.0,
+                style: BorderStyle.solid),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            borderSide: BorderSide(
+                color: Color.fromARGB(255, 149, 147, 150),
+                width: 2.0,
+                style: BorderStyle.solid),
+          ),
+        ),
+        keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
+      ),
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        debugPrint('pressed');
+      },
+      style: const ButtonStyle(
+        backgroundColor:  WidgetStatePropertyAll<Color>(Color.fromARGB(122, 132, 142, 142)),
+        foregroundColor: WidgetStatePropertyAll<Color>(Color.fromARGB(122, 132, 142, 142)),
+        fixedSize: WidgetStatePropertyAll<Size>(Size(100, 50)),
+      ),
+      child: const Text(
+        'convert',
+      ),
+    );
+  }
 }
